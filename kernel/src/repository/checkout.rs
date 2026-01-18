2dedf167bf16140c9164107281214f4e3f14efe9
Takahito Nakano <kano@ponko2.jp>
2026-01-18 23:55:26 +0900

    feat: Add chapter 5.6 code

---
+++ kernel/src/repository/checkout.rs
@@ -0,0 +1,18 @@
+use crate::model::{
+    checkout::{
+        Checkout,
+        event::{CreateCheckout, UpdateReturned},
+    },
+    id::{BookId, UserId},
+};
+use async_trait::async_trait;
+use shared::error::AppResult;
+
+#[async_trait]
+pub trait CheckoutRepository: Send + Sync {
+    async fn create(&self, event: CreateCheckout) -> AppResult<()>;
+    async fn find_history_by_book_id(&self, book_id: BookId) -> AppResult<Vec<Checkout>>;
+    async fn find_unreturned_all(&self) -> AppResult<Vec<Checkout>>;
+    async fn find_unreturned_by_user_id(&self, user_id: UserId) -> AppResult<Vec<Checkout>>;
+    async fn update_returned(&self, event: UpdateReturned) -> AppResult<()>;
+}
