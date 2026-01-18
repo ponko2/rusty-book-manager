9c4e41b54d9205375243f36d5845369f70ad1daa
Takahito Nakano <kano@ponko2.jp>
2026-01-19 01:32:37 +0900

    feat: Add chapter 6 code

---
+++ api/tests/api/book.rs
@@ -0,0 +1,114 @@
+use crate::{
+    deserialize_json,
+    helper::{TestRequestExt, fixture, make_router, v1},
+};
+use api::model::book::PaginatedBookResponse;
+use axum::{body::Body, http::Request};
+use kernel::{
+    model::{
+        book::Book,
+        id::{BookId, UserId},
+        list::PaginatedList,
+        user::BookOwner,
+    },
+    repository::book::MockBookRepository,
+};
+use rstest::rstest;
+use std::sync::Arc;
+use tower::ServiceExt;
+
+#[rstest]
+#[case("/books", 20, 0)]
+#[case("/books?limit=50", 50, 0)]
+#[case("/books?limit=50&offset=20", 50, 20)]
+#[case("/books?offset=20", 20, 20)]
+#[tokio::test]
+async fn show_book_list_with_query_200(
+    mut fixture: registry::MockAppRegistryExt,
+    #[case] path: &str,
+    #[case] expected_limit: i64,
+    #[case] expected_offset: i64,
+) -> anyhow::Result<()> {
+    let book_id = BookId::new();
+
+    fixture.expect_book_repository().returning(move || {
+        let mut mock = MockBookRepository::new();
+        mock.expect_find_all().returning(move |opt| {
+            let items = vec![Book {
+                id: book_id,
+                title: "RustによるWebアプリケーション開発".to_string(),
+                isbn: "".to_string(),
+                author: "Yuki Toyoda".to_string(),
+                description: "RustによるWebアプリケーション開発".to_string(),
+                owner: BookOwner {
+                    id: UserId::new(),
+                    name: "Yuki Toyoda".to_string(),
+                },
+                checkout: None,
+            }];
+            Ok(PaginatedList {
+                total: 1,
+                limit: opt.limit,
+                offset: opt.offset,
+                items,
+            })
+        });
+        Arc::new(mock)
+    });
+
+    let app: axum::Router = make_router(fixture);
+
+    let req = Request::get(v1(path)).bearer().body(Body::empty())?;
+    let resp = app.oneshot(req).await?;
+    assert_eq!(resp.status(), axum::http::StatusCode::OK);
+
+    let result = deserialize_json!(resp, PaginatedBookResponse);
+    assert_eq!(result.limit, expected_limit);
+    assert_eq!(result.offset, expected_offset);
+
+    Ok(())
+}
+
+#[rstest]
+#[case("/books?limit=-1")]
+#[case("/books?offset=aaa")]
+#[tokio::test]
+async fn show_book_list_with_query_400(
+    mut fixture: registry::MockAppRegistryExt,
+    #[case] path: &str,
+) -> anyhow::Result<()> {
+    let book_id = BookId::new();
+
+    fixture.expect_book_repository().returning(move || {
+        let mut mock = MockBookRepository::new();
+        mock.expect_find_all().returning(move |opt| {
+            let items = vec![Book {
+                id: book_id,
+                title: "RustによるWebアプリケーション開発".to_string(),
+                isbn: "".to_string(),
+                author: "Yuki Toyoda".to_string(),
+                description: "RustによるWebアプリケーション開発".to_string(),
+                owner: BookOwner {
+                    id: UserId::new(),
+                    name: "Yuki Toyoda".to_string(),
+                },
+                checkout: None,
+            }];
+            Ok(PaginatedList {
+                total: 1,
+                limit: opt.limit,
+                offset: opt.offset,
+                items,
+            })
+        });
+        Arc::new(mock)
+    });
+
+    let app: axum::Router = make_router(fixture);
+
+    let req = Request::get(v1(path)).bearer().body(Body::empty())?;
+    let resp = app.oneshot(req).await?;
+    assert_eq!(resp.status(), axum::http::StatusCode::BAD_REQUEST);
+
+    Ok(())
+}
