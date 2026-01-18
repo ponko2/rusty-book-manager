e574c65c3cd386e29bfd692cb64abd57d71db61d
Takahito Nakano <kano@ponko2.jp>
2026-01-18 21:03:26 +0900

    feat: Add chapter 5.3 code

--- kernel/src/repository/book.rs
+++ kernel/src/repository/book.rs
@@ -1,11 +1,13 @@
-use crate::model::book::{Book, event::CreateBook};
-use anyhow::Result;
+use crate::model::{
+    book::{Book, event::CreateBook},
+    id::BookId,
+};
 use async_trait::async_trait;
-use uuid::Uuid;
+use shared::error::AppResult;
 
 #[async_trait]
 pub trait BookRepository: Send + Sync {
-    async fn create(&self, event: CreateBook) -> Result<()>;
-    async fn find_all(&self) -> Result<Vec<Book>>;
-    async fn find_by_id(&self, book_id: Uuid) -> Result<Option<Book>>;
+    async fn create(&self, event: CreateBook) -> AppResult<()>;
+    async fn find_all(&self) -> AppResult<Vec<Book>>;
+    async fn find_by_id(&self, book_id: BookId) -> AppResult<Option<Book>>;
 }
