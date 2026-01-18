251b7890fc61276e2dd95bde71e805d3aa1c2233
Takahito Nakano <kano@ponko2.jp>
2026-01-18 22:12:02 +0900

    feat: Add chapter 5.4 code

---
+++ kernel/src/repository/user.rs
@@ -0,0 +1,19 @@
+use crate::model::{
+    id::UserId,
+    user::{
+        User,
+        event::{CreateUser, DeleteUser, UpdateUserPassword, UpdateUserRole},
+    },
+};
+use async_trait::async_trait;
+use shared::error::AppResult;
+
+#[async_trait]
+pub trait UserRepository: Send + Sync {
+    async fn create(&self, event: CreateUser) -> AppResult<User>;
+    async fn delete(&self, event: DeleteUser) -> AppResult<()>;
+    async fn find_all(&self) -> AppResult<Vec<User>>;
+    async fn find_current_user(&self, current_user_id: UserId) -> AppResult<Option<User>>;
+    async fn update_password(&self, event: UpdateUserPassword) -> AppResult<()>;
+    async fn update_role(&self, event: UpdateUserRole) -> AppResult<()>;
+}
