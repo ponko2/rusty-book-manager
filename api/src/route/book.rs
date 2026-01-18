37015d9628c204d957392c6051411edec786b791
Takahito Nakano <kano@ponko2.jp>
2026-01-18 23:37:48 +0900

    feat: Add chapter 5.5 code

--- api/src/route/book.rs
+++ api/src/route/book.rs
@@ -1,16 +1,17 @@
+use crate::handler::book::{delete_book, register_book, show_book, show_book_list, update_book};
 use axum::{
     Router,
-    routing::{get, post},
+    routing::{delete, get, post, put},
 };
 use registry::AppRegistry;
-
-use crate::handler::book::{register_book, show_book, show_book_list};
 
 pub fn build_book_routers() -> Router<AppRegistry> {
     let books_routers = Router::new()
         .route("/", post(register_book))
         .route("/", get(show_book_list))
-        .route("/:book_id", get(show_book));
+        .route("/:book_id", get(show_book))
+        .route("/:book_id", put(update_book))
+        .route("/:book_id", delete(delete_book));
 
     Router::new().nest("/books", books_routers)
 }
